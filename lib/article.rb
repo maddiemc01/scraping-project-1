18fc04dd0810046003129c523dc0bdedbaaf5d8f
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-19 14:15:31 -0400

    fix rubo and pseudo notes

--- lib/article.rb
+++ lib/article.rb
@@ -1,20 +1,17 @@
-#blueprint of object (Fashion show)
-#no puts, just stores instances of data i.e. attributes
+# blueprint of object
+# no puts, just stores instances of data i.e. attributes of articles
 
 class Article
   attr_accessor :title, :author, :url, :date, :biopart1, :biopart2, :id
 
   @@all = []
-  @@current_id = 1
 
   def initialize(title = nil, author = nil, url = nil)
     @title = title
     @author = author
     @url = url
-    @date
-    @id = @@current_id
     @@all << self
-    @@current_id += 1
+    @id = @@all.count
   end
 
   def self.all
