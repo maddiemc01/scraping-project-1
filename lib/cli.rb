c0982a8ba3aa681a194c7c1abc16a9662a4455d1
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-19 11:01:13 -0400

    create rough user interface

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,24 +9,33 @@ class Cli
     Scraper.new
     puts "Welcome fashionista/o!"
     puts "Here you can stay up to date with all the latest fashion trends."
-    #start
+    start
   end
 
   def start
     article_list
-    puts "Choose an article by typing the article ID number it is associated with."
-    puts "If you would like to exit, type 'exit'"
-    input = gets.strip.downcase
-    if input == "exit"
-      puts "thanks for browsing, see you later!"
-      exit
-    elsif
-
-
+    while true do
+      puts "Choose an article by typing the article ID number it is associated with."
+      puts "If you would like to exit, type 'exit'"
+      input = gets.strip.downcase
+      if input == "exit"
+        puts "Thanks for browsing, see you later!"
+        exit
+      elsif input.to_i <=0 || input.to_i > Article.all.count
+        binding.pry
+        puts "Sorry, there is currntly no article associated with that ID, try entering a different ID:"
+      elsif input
+        article = Article.find_by_id(input.to_i)
+        Scraper.new.scrape_article(article)
+        puts "#{article.title} was written on #{article.date}."
+        puts "Here's a quick abstract: '#{article.bio}'"
+        puts "if interested in the full article, visit: #{article.url}"
+      end
+    end
   end
 
   def article_list
-    puts "Current Articles:"
+    puts "Current Articles to Explore:"
     Article.all.map do |art|
       puts "* Article ID: #{art.id}"
       puts "* Name: #{art.title}"
@@ -34,9 +43,6 @@ class Cli
       puts "*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*"
     end
   end
-
-  def make_selection
-    puts"
 
 end
 
