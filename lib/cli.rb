c110d63e93d745107ad01d74b7a08178fc6ad816
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-19 11:43:53 -0400

    fine tune cli look

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,22 +14,26 @@ class Cli
 
   def start
     article_list
-    while true do
-      puts "Choose an article by typing the article ID number it is associated with."
+      puts "Choose an article by entering the article ID number it is associated with."
+      while true do
       puts "If you would like to exit, type 'exit'"
       input = gets.strip.downcase
       if input == "exit"
         puts "Thanks for browsing, see you later!"
         exit
       elsif input.to_i <=0 || input.to_i > Article.all.count
-        binding.pry
-        puts "Sorry, there is currntly no article associated with that ID, try entering a different ID:"
+        puts "Sorry, there is currently no article associated with that ID, try entering a different ID:"
       elsif input
         article = Article.find_by_id(input.to_i)
         Scraper.new.scrape_article(article)
-        puts "#{article.title} was written on #{article.date}."
-        puts "Here's a quick abstract: '#{article.bio}'"
-        puts "if interested in the full article, visit: #{article.url}"
+        puts "You've chosen '#{article.title}', which was written on #{article.date}."
+        puts ""
+        puts "To give you an idea of what this article is about, here's an abstract:"
+        puts "    #{article.biopart1}"
+        puts "    #{article.biopart2}"
+        puts "If interested in the full article, visit: #{article.url}"
+        puts ""
+        puts "If you'd like to look at a different article, enter that article's ID number."
       end
     end
   end
